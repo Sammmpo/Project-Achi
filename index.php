ceccf320c0e03d630cbab724fc7e66c305084b52
Sammmpo <sampsa.kares@pp.inet.fi>
2017-02-04 00:54:20 +0200

    update

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includer.php';?>
+
+<title>Hobbyton</title>
+
+<head>
+
+</head>
+
+
+<body>
+
+<div class="container">
+
+<div class="space"></div>
+
+<div class="col-lg-4 col-md-3 col-sm-2 col-xs-0">
+
+</div>
+
+<div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 whitebg">
+
+  <div class="title">
+      <h1>Hobbyton</h1>
+      <br>
+      <h2>They are taking the hobbies to YOU!</h2>
+</div>
+
+    <form action="account.php" method="post">
+      <div class="form-group">
+        <input type="email" class="form-control" id="email" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" id="pwd" placeholder="Password">
+      </div>
+      <div class="checkbox">
+      </div>
+      <button type="login" class="btn btn-block">Login</button>
+      </form>
+      <br><br>
+    <span class="text">Not a member?</span><br><br><a href="register.php">Register now</a>
+      <br><br><br>
+
+
+
+</div>
+
+<div class="col-lg-4 col-md-3 col-sm-2 col-xs-0">
+</div>
+
+<div class="space"></div>
+
+</div>
+
+</body>
+
+
+</html>
